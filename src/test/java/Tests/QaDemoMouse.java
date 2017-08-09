27c0a74c103309f50252b8cca666198fe18d133a
Deaglan <DeaglanLynch@hotmail.co.uk>
2017-08-09 16:14:34 +0100

    Inital commit

---
+++ src/test/java/Tests/QaDemoMouse.java
@@ -0,0 +1,38 @@
+package Tests;
+
+import org.junit.*;
+import org.junit.internal.runners.statements.Fail;
+
+public class QaDemoMouse {
+
+
+    @BeforeClass
+    public static void beforeClass()
+    {
+
+    }
+    @Before
+    public void beforeT()
+    {
+
+    }
+    @Test
+    public void test1()
+    {
+        Assert.fail("Not implemented");
+
+    }
+    @After
+    public void afterT()
+    {
+
+    }
+    @AfterClass
+    public static void afterClass()
+    {
+
+    }
+}
+
+
+
